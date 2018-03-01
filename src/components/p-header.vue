e50e5018bc77ad2c823d3d339c863bcb3e9eab69
weimin <weimin19940508@126.com>
2018-03-01 23:52:09 +0800

    [update] pHeader上的按钮

--- src/components/p-header.vue
+++ src/components/p-header.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="p-header">
-
+     <el-button round class="new-article" @click="newArticle">新建文章</el-button>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'p-header',
+    methods: {
+      newArticle () {
+        // this.$router.push('/article/new')
+      }
+    }
+  }
+</script>
 
 <style lang="stylus">
   sky_blue =	#87CEFA
 
-  .p-header
+  .p-header {
     flex 0 0 auto
     width 100%
     height 50px
     background skyBlue
+    .new-article {
+      position absolute
+      top 5px
+      right 50px
+      &:hover {
+        pointer cursor
+      }
+    }
+  }
 </style>
